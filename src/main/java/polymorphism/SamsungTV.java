323c3035109b1d364cac90364cd628d10735d0c3
ChickenPaella <lhbv1421@hanmail.net>
2018-01-21 17:36:07 +0900

    setter method 설정, 인터페이스 추출

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,28 +1,35 @@
 package polymorphism;
 
-public class SamsungTV implements TV{
+public class SamsungTV implements TV {
+
+	private Speaker speaker;
+	private int price;
+
+	public SamsungTV() {
+		System.out.println("Samsung TV---매개변수 없는 생성");
+	}
 	
-	public void initMethod() {
-		System.out.println("객체 생성");
+	public void setSpeaker(Speaker speaker) {
+		this.speaker = speaker;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
 	}
 
-	public void destroyMethod() {
-		System.out.println("객체 소멸");
+	public void volumeUp() {
+		speaker.volumeUp();
 	}
-	
+
+	public void volumeDown() {
+		speaker.volumeDown();
+	}
+
 	public void powerOn() {
-		System.out.println("SamsungTV--전원 켠다.");
+		System.out.println("SamsungTV--전원 켠다. 가격 : " + price + "원");
 	}
 
 	public void powerOff() {
 		System.out.println("SamsungTV--전원 끈다.");
-	}
-
-	public void volumeUp() {
-		System.out.println("SamsungTV--소리 올린다.");
-	}
-
-	public void volumeDown() {
-		System.out.println("SamsungTV--소리 내린다.");
 	}
 }
