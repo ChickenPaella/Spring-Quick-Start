93022e600a860f2a5ca79940fa1833156c2fcd17
ChickenPaella <lhbv1421@hanmail.net>
2018-01-30 22:40:48 +0900

    Spring DispatcherServlet 적용

--- src/main/java/com/springbook/view/board/GetBoardListController.java
+++ src/main/java/com/springbook/view/board/GetBoardListController.java
@@ -6,22 +6,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
 public class GetBoardListController implements Controller{
 
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 목록 검색 처리");
 		
 		BoardDAO boardDAO = new BoardDAO();
 		List<BoardVO> boardList = boardDAO.getBoardList();
 		
-		HttpSession session = request.getSession();
-		session.setAttribute("boardList", boardList);
-		return "getBoardList";
+//		HttpSession session = request.getSession();
+//		session.setAttribute("boardList", boardList);
+//		return "getBoardList";
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("boardList", boardList);
+		mav.setViewName("getBoardList");
+		return mav;
 	}
 
 }
